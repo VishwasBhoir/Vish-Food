4ac1e68bc67d79fb32f24f05c3d9b13263c9503e
VISHWAS BHOIR <vishwas.11challengers@gmail.com>
2024-02-19 15:31:23 +0530

    Initial Setup

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello From ReactJS!");
+const heading = React.createElement(
+  "h1",
+  { className: "heading", id: "heading" },
+  "Hello from React.!"
+);
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
