ab670a2962084f8569181b94a166b1f163aec374
VISHWAS BHOIR <vishwas.11challengers@gmail.com>
2024-02-19 20:10:12 +0530

    Configured Parcel

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement(
-  "h1",
+  "div",
   { className: "heading", id: "heading" },
-  "Hello from React.!"
+  [
+    React.createElement("h1", {}, "Hello From React!"),
+  ]
 );
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
